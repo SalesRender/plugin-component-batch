3b0a61367ad603f4084df831738bd6fc85fef2db
XAKEPEHOK <icq-475204@ya.ru>
2020-12-02 16:52:21 +0300

    Added BatchHandler

---
+++ src/BatchHandler.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created for plugin-component-batch
+ * Date: 02.12.2020
+ * @author Timur Kasumov (XAKEPEHOK)
+ */
+
+namespace Leadvertex\Plugin\Components\Batch;
+
+
+use RuntimeException;
+
+final class BatchHandler
+{
+
+    private static BatchHandlerInterface $handler;
+
+    private function __construct() {}
+
+    public function config(BatchHandlerInterface $handler): void
+    {
+        self::$handler = $handler;
+    }
+
+    public function getInstance(): BatchHandlerInterface
+    {
+        if (!isset(self::$handler)) {
+            throw new RuntimeException('Batch form registry was not configured');
+        }
+
+        return self::$handler;
+    }
+
+}
